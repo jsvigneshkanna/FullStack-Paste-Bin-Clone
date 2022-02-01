7b16c628de8c0c164ac80d6935cc1e31db21b4c5
vigneshkanna <vkrishitaran@gmail.com>
2022-02-02 00:54:24 +0530

    api folder cleaned

--- pastebin/pages/api/snippets.js
+++ pastebin/pages/api/snippets.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-export default function (req, res) {
-  // Creating new moongoose object and pushing to supabase
-  const dbUrl = process.env.SUPABASE_URL;
-  console.log("fafaf", dbUrl);
-  res.status(200).json({ name: dbUrl });
-}
+// export default function (req, res) {
+//   // Creating new moongoose object and pushing to supabase
+//   const dbUrl = process.env.SUPABASE_URL;
+//   console.log("fafaf", dbUrl);
+//   res.status(200).json({ name: dbUrl });
+// }
