b6c926e352bdc29093858659e2195a31b760ab1b
vigneshkanna <vkrishitaran@gmail.com>
2022-02-03 00:27:28 +0530

    own bins link added nav

--- pastebin/components/Navbar.js
+++ pastebin/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/Ownbins">
+                <a className="nav-link" href="/snippetDisplay">
                   Own Bins
                 </a>
               </li>
